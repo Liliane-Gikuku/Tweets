fc907717e1f027ac187ca92a2267999167c26833
Liliane-Gikuku <lilkundiro@gmail.com>
2022-07-09 09:53:35 +0300

    icons

--- app/src/main/java/com/liliane/twitterpost/MainActivity.kt
+++ app/src/main/java/com/liliane/twitterpost/MainActivity.kt
@@ -17,12 +17,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun postTweets(){
-        var tweet=Tweet("Liliane","@lilkundiro","4hr","Is it a sin to be seen?","60","40","900k")
-        var tweet2=Tweet("Liliane","@lilkundiro","4hr","Is it a sin to be seen?","60","40","900k")
-        var tweet3=Tweet("Liliane","@lilkundiro","4hr","Is it a sin to be seen?","60","40","900k")
-        var tweet4=Tweet("Liliane","@lilkundiro","4hr","Is it a sin to be seen?","60","40","900k")
-        var tweet5=Tweet("Liliane","@lilkundiro","4hr","Is it a sin to be seen?","60","40","900k")
-        var tweet6=Tweet("Liliane","@lilkundiro","4hr","Is it a sin to be seen?","60","40","900k")
+        var tweet=Tweet("Liliane","@lilkundiro","4hr","Happy birthday to my puppy!","60","40","900k")
+        var tweet2=Tweet("Liliane","@lilkundiro","4hr","Happy birthday to my puppy!","60","40","900k")
+        var tweet3=Tweet("Liliane","@lilkundiro","4hr","Happy birthday to my puppy!","60","40","900k")
+        var tweet4=Tweet("Liliane","@lilkundiro","4hr","Happy birthday to my puppy!","60","40","900k")
+        var tweet5=Tweet("Liliane","@lilkundiro","4hr","Happy birthday to my puppy!","60","40","900k")
+        var tweet6=Tweet("Liliane","@lilkundiro","4hr","Happy birthday to my puppy!","60","40","900k")
 
         var tweets= listOf(tweet,tweet2,tweet3,tweet4,tweet5,tweet6)
 
